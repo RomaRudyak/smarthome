f5e4f26f8ae5fd55a4c0773ca36bdcaef8b2378d
Roma.Rudyak <roma.rudyak@gmail.com>
2024-03-06 14:54:17 +0200

    Add PCD844 lcd device

--- RORU.SmartHome/Program.cs
+++ RORU.SmartHome/Program.cs
@@ -1,21 +1,84 @@
-﻿using nanoFramework.Hardware.Esp32;
-using RORU.SmartHome;
-using System;
+﻿using System;
+using System.Device.Gpio;
 using System.Device.I2c;
+using System.Device.Spi;
+using System.Diagnostics;
 using System.Threading;
 
-Configuration.SetPinFunction(21, DeviceFunction.I2C1_DATA);
-Configuration.SetPinFunction(22, DeviceFunction.I2C1_CLOCK);
+using Iot.Device;
+
+using nanoFramework.Hardware.Esp32;
+
+using RORU.SmartHome;
+
+const int sleepTime = 60 * 1000;
+//const int sleepTime = 1000;
+
+Configuration.SetPinFunction(Gpio.IO21, DeviceFunction.I2C1_DATA);
+Configuration.SetPinFunction(Gpio.IO22, DeviceFunction.I2C1_CLOCK);
+
+Configuration.SetPinFunction(Gpio.IO18, DeviceFunction.SPI1_CLOCK);
+Configuration.SetPinFunction(Gpio.IO23, DeviceFunction.SPI1_MOSI);
+
+var dataCommandPin = Gpio.IO04;
+var resetPin = Gpio.IO19;
+
+SpiConnectionSettings spiConnectionSettings = new(1, 5)
+{
+    ClockFrequency = 5_000_000,
+    Mode = SpiMode.Mode0,
+    DataFlow = DataFlow.MsbFirst,
+    ChipSelectLineActiveState = PinValue.Low
+};
+var spiDevice = new SpiDevice(spiConnectionSettings);
+
+Pcd8544 lcd = new(dataCommandPin, spiDevice, resetPin, -1, null, false)
+{
+    Enabled = true
+};
 
 I2cDevice d = I2cDevice.Create(new(1, Sht21.I2cAddress));
 using Sht21 sensor = new(d);
 
 while (true)
 {
-    Console.WriteLine(new string('=', 10));
-    Console.WriteLine($"Temperature:\t {sensor.Temperature.DegreesCelsius:F0}\u2103");
-    Console.WriteLine($"Humidity:\t\t {sensor.Humidity.Percent:F0}%");
-    Console.WriteLine(new string('=', 10));
+    try
+    {
+        ClearOutput();
 
-    Thread.Sleep(2000);
+        var temp = sensor.Temperature.DegreesCelsius;
+        var hum = sensor.Humidity.Percent;
+
+        var tempStatus = IsTempGood(temp) ? "Good" : "Not good";
+        WriteLine("Temperature:");
+        WriteLine($" {temp:F0} C");
+        WriteLine($" {tempStatus}");
+
+        var humStatus = IsHumGood(hum) ? "Good" : "Not good";
+        WriteLine("Humidity:");
+        WriteLine($" {hum:F0} %");
+        WriteLine($" {humStatus}");
+
+        Thread.Sleep(sleepTime);
+    }
+    catch (Exception e)
+    {
+        Debug.WriteLine(e.Message);
+    }
+}
+
+bool IsHumGood(double hum) => hum >= 50 && hum <= 70;
+
+bool IsTempGood(double temp) => temp >= 18 && temp <= 22;
+
+void WriteLine(string line = null)
+{
+    var v = line ?? string.Empty;
+    lcd.WriteLine(v);
+    Debug.WriteLine(v);
+}
+
+void ClearOutput()
+{
+    lcd.Clear();
 }
